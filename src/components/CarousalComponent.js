b759ccaab4cb80a88920848176e97214c2353026
mehak-droot <mehak.dhawan@droot.in>
2023-03-01 19:22:17 +0530

    --waitlist page, image resp---

--- src/components/CarousalComponent.js
+++ src/components/CarousalComponent.js
@@ -11,17 +11,13 @@ function ControlledCarousel() {
     };
   
     return (
-      <Carousel activeIndex={index} onSelect={handleSelect} indicators={false} >
+      <Carousel activeIndex={index} onSelect={handleSelect}  controls={false} >
         <Carousel.Item>
           <img
             className="d-block w-100 "
             src={image1}
             alt="First slide"
           />
-          {/* <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption> */}
         </Carousel.Item>
         <Carousel.Item>
           <img
@@ -29,11 +25,6 @@ function ControlledCarousel() {
             src={image1}
             alt="Second slide"
           />
-  
-          {/* <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption> */}
         </Carousel.Item>
         <Carousel.Item>
           <img
@@ -41,13 +32,6 @@ function ControlledCarousel() {
             src={image1}
             alt="Third slide"
           />
-  
-          {/* <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption> */}
         </Carousel.Item>
       </Carousel>
     );
